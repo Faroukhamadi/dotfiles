8bcc6108470760937235efdacc68e59dbf45f393
faroukhamadi <faroukhamadi1@outlook.com>
2022-12-30 09:21:13 +0100

    add copies of the files rather than symlinks

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias c="code"
+alias p="pnpm"
+
+alias s="sudo apt install"
+alias md="mkdir"
+alias py="python3"
+alias g="git"
+alias sf="source ~/.config/fish/config.fish"
+alias e="echo"
+alias cs="cd programming/cs229-2018-autumn/"
+neofetch
+
+alias stop-echo="pactl unload-module module-loopback"
+alias start-echo="pactl load-module module-loopback latency_msec=0"
